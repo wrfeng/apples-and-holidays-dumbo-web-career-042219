407ca2c7055f9cc7a1685110681651a0d81e8c94
wei feng <theweifeng@gmail.com>
2019-04-14 16:55:24 +0000

    Automatically backed up by Learn

--- spec/holiday_spec.rb
+++ spec/holiday_spec.rb
@@ -125,18 +125,18 @@ TEXT
     # of holiday supplies line by line. If, on the other hand, you decided to output it
     # as one big chunk, comment out this test, and uncomment the one below it.
 
-    # it "should output the formatted list of holidays and their supplies" do
-    #   @output.each_line do |line|
-    #     expect($stdout).to receive(:puts).with(line.chomp)
-    #   end
-
-    #   all_supplies_in_holidays(holiday_supplies)
-    # end
-
     it "should output the formatted list of holidays and their supplies" do
-      expect($stdout).to receive(:puts).with(@output)
+      @output.each_line do |line|
+        expect($stdout).to receive(:puts).with(line.chomp)
+      end
+
       all_supplies_in_holidays(holiday_supplies)
     end
+
+    # it "should output the formatted list of holidays and their supplies" do
+    #   expect($stdout).to receive(:puts).with(@output)
+    #   all_supplies_in_holidays(holiday_supplies)
+    # end
 
   end
 
